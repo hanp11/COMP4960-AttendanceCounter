a935fee7f5cab60ffca54771d319ad1996cb7dd3
Rob Hird <robertjhird@gmail.com>
2019-02-24 22:28:18 -0500

    added functionality to display Database data in option tags

--- django_project/boston_code_camp/models.py
+++ django_project/boston_code_camp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    RoomName = models.CharField(max_length=100)
+    Capacity = models.CharField(max_length=15)
+
+class Speaker(models.Model):
+    FirstName = models.CharField(max_length=100)
+    LastName = models.CharField(max_length=100)
+    Email = models.CharField(max_length=100)
+
+class TimeSlot(models.Model):
+    StartTime = models.CharField(max_length=15)
+    EndTime = models.CharField(max_length=15)
+    Duration = models.CharField(max_length=15)
+
+class Session(models.Model):
+    SessionName = models.CharField(max_length=200)
+    RoomName = models.CharField(max_length=100)
+    Speaker = models.CharField(max_length=100)
+    TimeSlot = models.CharField(max_length=100)
