2987856a0a12d4a2cb1521f2ba253d75c9931998
Letendre <letendres@wit.edu>
2019-04-07 22:01:21 -0400

    user login json

--- routes/user/userlogin.js
+++ routes/user/userlogin.js
@@ -1,65 +1,26 @@
 const fs = require('fs');
 
+var contents = fs.readFileSync("daily-password.json");
+var jsoncontents = JSON.parse(contents);
+
+
 module.exports = {
     userLoginPage: (req, res) => {
-        let query = "SELECT * FROM `room`"; // query database to get all the Rooms
-        // execute query
-        db.query(query, (err, result) => {
-            if (err) {
-                res.redirect('/');
-            }
         res.render('userlogin.ejs', {
             title: "Add Room"
-            ,message: ''
-            ,room: result
             });
-        });
     },
 
     userLogin: (req, res) => {
 
         let message = '';
-        let room_name = req.body.room_name;
-        let capactity = req.body.capacity_id;
+        let username = req.body.username;
+        let password = req.body.password;
 
-        let existingRoomQuery = "SELECT * FROM `room` WHERE RoomName = '" + room_name + "'";
+        let daily_password = JSON.stringify(jsoncontents.daily_password);
 
-        db.query(existingRoomQuery, (err, result) => {
-            if (err) {
-                return res.status(500).send(err);
-            }
-            if (result.length > 0) {
-                message = 'Room Name already exists';
-                res.render('userlogin.ejs', {
-                    message,
-                    title: "Add Room"
-                });
-                
-            } else {
-                // send the room's details to the database
-                let query = "INSERT INTO `room` (RoomName, Capacity) VALUES ('" +
-                    room_name + "', '" + capactity +"')";
-                db.query(query, (err, result) => {
-                    if (err) {
-                        return res.status(500).send(err);
-                    }
-                    // res.redirect('/');
-                    query = "SELECT * FROM `room`"; // query database to get all the Rooms
-                    // execute query
-                    db.query(query, (err, result) => {
-                        if (err) {
-                            res.redirect('/');
-                        }
-                    res.render('userlogin.ejs', {
-                        title: "Add Room"
-                        ,message: 'Room Added'
-                        ,room: result
-                        });
-                    });
-                });
-                    
-            }
-        });
+        if(password === daily_password){
+            res.redirect('/index.ejs');
+        }
     },
-
 };
